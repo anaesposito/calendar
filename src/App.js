701df30e2237deec9619c0589d6136cc1727bca6
anaesposito <anaclaraesposito@gmail.com>
2022-07-22 21:32:33 -0300

    add event function working ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,38 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import FullCalendar from "@fullcalendar/react";
+import daygridPlugin from "@fullcalendar/daygrid";
 import "./index.css";
-const App = () => <></>;
+import AddEventModal from "./components/AddEventModal";
+
+const App = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+  const calendarRef = useRef(null);
+
+  const onEventAdded = (event) => {
+    let calendarAPI = calendarRef.current.getApi();
+    calendarAPI.addEvent(event);
+  };
+
+  console.log("holaa app.js");
+
+  return (
+    <div>
+      <button onClick={() => setModalOpen(true)}>Add Event</button>
+
+      <div style={{ position: "relative", zIndex: "0" }}>
+        <FullCalendar
+          ref={calendarRef}
+          plugins={[daygridPlugin]}
+          initialView="dayGridMonth"
+        />
+      </div>
+
+      <AddEventModal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onEventAdded={(event) => onEventAdded(event)}
+      />
+    </div>
+  );
+};
 export default App;
